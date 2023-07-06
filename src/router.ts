72c94d8946523c84baab449086124e8e6f97b9cd
Toheeb Ojuolape <45619240+Toheeb-Ojuolape@users.noreply.github.com>
2023-07-06 18:52:43 +0100

    feat: added api for withdrawal with lightning invoice

--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,11 @@
 const { Router } = require('express');
+import { lninvoiceWithdrawal } from "./controllers/lninvoiceWithdrawController";
 import { lnurlWithdrawal } from "./controllers/lnurlwithdrawController";
 
 
 const router = Router();
 
 router.post("/lnurl-withdraw",lnurlWithdrawal)
+router.post("/invoice-withdraw",lninvoiceWithdrawal)
 
 module.exports = router
